178c92036d8085b8bc685c77adffd864ecf29d78
Neha O7 <neha@o7tec.com>
2024-06-14 17:56:45 +0530

    14 June 2024
    Implicit intent
    Activity Lifecycle

--- app/src/main/java/com/o7services/kotlin5_6_online/ActivityLifecycle.kt
+++ app/src/main/java/com/o7services/kotlin5_6_online/ActivityLifecycle.kt
@@ -1,12 +1,20 @@
 package com.o7services.kotlin5_6_online
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class ActivityLifecycle : AppCompatActivity() {
+
+    var btn:Button?=null
+    var btn2:Button?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +24,48 @@ class ActivityLifecycle : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        btn=findViewById(R.id.btnImplicit)
+        btn?.setOnClickListener {
+            var intent=Intent(Intent.ACTION_VIEW)
+            intent.setData(Uri.parse("https://o7services.com/"))
+            startActivity(intent)
+        }
+        btn2=findViewById(R.id.btnImplicit2)
+        btn2?.setOnClickListener {
+            var intent = Intent(Intent.ACTION_DIAL)
+                intent.setData(Uri.parse("tel:9492924601"))
+                startActivity(intent)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(this, "On Start", Toast.LENGTH_SHORT).show()
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(this, "On Pause", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(this, "On Resume", Toast.LENGTH_SHORT).show()
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Toast.makeText(this, "On Stop", Toast.LENGTH_SHORT).show()
+
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(this, "On Destroy", Toast.LENGTH_SHORT).show()
+
     }
 }
